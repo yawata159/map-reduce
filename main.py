04118614b189387b5472eda1181e5bac7045dd51
yawata159 <seijiyawata@gmail.com>
2017-05-02 19:00:49 -0400

    frequency stuff

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import string
+
 with open('book.txt') as f:
-    word_list = f.read().decode('utf-8-sig').encode('utf-8').strip().split()
+    word_list = (f.read()
+            .lower()
+            .decode('utf-8-sig')
+            .encode('utf-8')
+            .strip()
+            .translate(string.maketrans(string.punctuation, 32*' '))
+            .split())
 
 def word_freq(word):
-    pass
+    return len(filter(lambda s: s == word, word_list))
 
 def words_freq(words):
-    pass
+    return reduce(lambda a,b: a+b, map(lambda s: 1 if s in words else 0, word_list))
 
 def most_freq():
     pass
+
+if __name__ == '__main__':
+
