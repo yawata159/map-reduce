8949145321fac1376581fecabc176b698f5b673b
yawata159 <seijiyawata@gmail.com>
2017-05-02 14:49:18 -0400

    fxns

---
+++ main.py
@@ -0,0 +1,11 @@
+with open('book.txt') as f:
+    word_list = f.read().decode('utf-8-sig').encode('utf-8').strip().split()
+
+def word_freq(word):
+    pass
+
+def words_freq(words):
+    pass
+
+def most_freq():
+    pass
